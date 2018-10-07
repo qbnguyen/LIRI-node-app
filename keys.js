9cb7ca434daa382754581cb35090c3cdf6ac1a67
Quy Nguyen <nguyenqb24@gmail.com>
2018-10-06 22:39:46 -0400

    few bugs left, working on it

--- keys.js
+++ keys.js
@@ -1,17 +1,7 @@
 console.log('this is loaded');
-var Spotify = require('node-spotify-api');
+
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
-var spotify = new Spotify(keys.spotify);
-
-// spotify
-//   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-//   .then(function(data) {
-//     console.log(data); 
-//   })
-//   .catch(function(err) {
-//     console.error('Error occurred: ' + err); 
-//   });
--- liri.js
+++ liri.js
@@ -1,49 +1,83 @@
-// require("dotenv").config();
+require("dotenv").config();
+var keys = require("./keys.js")
 var fs = require("fs");
 var axios = require("axios");
-var inputs = process.argv[2]
+var moment = require("moment");
+var Spotify = require("node-spotify-api");
+
+var actions = process.argv[2];
+var title = process.argv.slice(3).join(" ");
+
+
+
 //add the code required to import the keys.js file and store it in a variable
 // You should then be able to access your keys information like so
-switch(inputs){
-  case "concert-this":
-    break;
-  case "spotify-this-song":
-      spotify();
-    break;
-  case "movie-this":
-      movie();
-    break;
-  case "do-what-this-says":
-    break;
+function bands(){
+  var queryUrl = "https://rest.bandsintown.com/artists/" + title + "/events?app_id=codingbootcamp";
+  axios.get(queryUrl).then(
+    function (response){
+      console.log(response.data);
+      if (response.data.length == 0){
+        console.log("sorry try again");
+        return;
+      }
+      console.log("concerts for " + title);
+      for (var i =0 ; i < response.data.length; i++){
+      console.log(response.data[i].venue.city + "," + (response.data[i].venue.region || response.data[i].venue.country) + " at " + response.data[i].venue.name + " on the following day " + moment(response.data[i].datetime).format("MM/DD/YYYY"))
+    }
+  }
+  );
+
 }
 
 function spotify(){
 var spotify = new Spotify(keys.spotify);
-
+if (!title){
+  title = "All The Small Things";
+}
 spotify
-  .search({ type: 'track', query: 'All the Small Things' })
-  .then(function(response) {
-    console.log(response);
-  })
-  .catch(function(err) {
-    console.log(err);
+  .search({ type: 'track', query: title }, function (error, data){
+    if (error){
+      
+      return;
+    }
+    var songs = data.tracks.items;
+    
+    for (var i= 0; i < 5; i++){
+      console.log(songs[i].name);
+      var artistArray = songs[i].artists;
+      var artists = [];
+      for (var j = 0 ; j < artistArray.length; j++){
+        artists.push(artistArray[i].name);
+      }
+      console.log("artists: " + artists.join(", "));
+      if (songs[i].preview_url){
+        console.log("preview: " + songs[i].preview_url);
+      }
+      console.log("album name: " + songs[i].album.name);
+      console.log("--------------------------");
+    }
   });
+  
 }
 
 
 function movie(){
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+  
+var queryUrl = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
 
 console.log(queryUrl);
 
-if (movieName == ""){
-  console.log("http://www.omdbapi.com/?t=" + Mr.Nobody + "&y=&plot=short&apikey=trilogy")
-  console.log(response.data)
+if (!title) {
+title = "Mr Nobody";
+  console.log("If you haven't watched Mr. Nobody, then you should: http://www.imdb.com/title/tt0485947/");
+	 	console.log("It's on Netflix!")
 }
 axios.get(queryUrl).then(
   
     function(response) {
-    console.log(response.data)
+      
+    // console.log(response.data)
     console.log("Title: "+ response.data.Title);
     console.log("Release Year: " + response.data.Year);
     console.log("Ratings :"+ response.data.imbdRating);
@@ -52,12 +86,16 @@ axios.get(queryUrl).then(
     console.log("Plot: "+ response.data.Plot)
     console.log("Actors: " + response.data.Actors)
     console.log("Country: " + response.data.Country)
-    }
+
+    if (response.data.Ratings[1]) {
+      console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+        }
+      }
   
-);
-  }
+  );
+}
 
-
+function random(){
 
 fs.readFile("random.txt", "utf8", function(error, data) {
 
@@ -66,14 +104,31 @@ fs.readFile("random.txt", "utf8", function(error, data) {
     return console.log(error);
   }
 
-  // We will then print the contents of data
-  else {
-    console.log(data);
-
   // Then split it by commas (to make it more readable)
   var dataArr = data.split(",");
-
+  actions = dataArr[0];
+  title = dataArr[1];
   // We will then re-display the content as an array for later use.
-  console.log(dataArr);
-  } 
+  runActions();
+   
 });
+}
+function runActions(){
+switch(actions){
+  case "concert-this":
+      bands();
+    break;
+  case "spotify-this-song":
+      spotify();
+    break;
+  case "movie-this":
+      movie();
+    break;
+  case "do-what-this-says":
+      random();
+    break;
+  default:
+    console.log("Pick one of the commands!");
+}
+}
+runActions();
